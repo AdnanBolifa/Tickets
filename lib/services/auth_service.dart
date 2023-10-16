b90f4b7f477271603388fa5386540f9dcff6692d
TheF0x <retr0f0x.f0@gmail.com>
2023-10-16 07:24:43 -0700

    Remove un-used lines

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthService {
   static const apiUrl = 'https://reqres.in/api/login';
-  static const apiUrlFetch = 'https://reqres.in/api/users/2';
 
   Future<String> login(String email, String password) async {
     //Including the Token in Request Headers:
@@ -40,19 +37,5 @@ class AuthService {
   Future<void> logout() async {
     // Remove the token when the user logs out.
     await storage.delete(key: 'jwt_token');
-  }
-
-  Future<Map<String, dynamic>> fetchData() async {
-    final response = await http.get(Uri.parse(apiUrlFetch));
-
-    if (response.statusCode == 200) {
-      // Successfully logged in, parse the JSON response and return a map.
-      Map<String, dynamic> data = json.decode(response.body);
-      print(response.body);
-      return data;
-    } else {
-      // Handle login failure.
-      throw Exception('Failed to retrieve the data');
-    }
   }
 }
